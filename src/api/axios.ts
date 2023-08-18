385f64ea3dc6eb7819bba20a1c0d81cc8b1cb8d5
Hong Xuan <xuanvan229@gmail.com>
2023-08-18 17:36:17 +0700

    update env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+// read from .env file
+const baseURL = import.meta.env.VITE_BASE_URL || "http://localhost:8080";
 export const instance = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL,
   timeout: 10000,
 });
