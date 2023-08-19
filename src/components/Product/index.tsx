9c520f0770639c86d6424b07ff8824b16f463cf6
Hong Xuan <xuanvan229@gmail.com>
2023-08-19 10:25:48 +0700

    update ui

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -15,19 +15,21 @@ export const Product = ({ product }: { product: IProduct }) => {
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
       <Card
         onClick={onClick}
-        className="cursor-pointer h-full overflow-hidden hover:shadow-lg"
+        className="cursor-pointer h-full overflow-hidden hover:shadow-lg flex flex-col"
       >
         <img
-          className="w-full h-[300px] object-cover"
+          className="w-full h-[200px] sm:h-[250px] lg:h-[300px] object-cover border-b shadow-xs"
           src={product.image}
           alt=""
         />
         <CardHeader>
           <CardTitle>{product.name}</CardTitle>
-          <CardDescription>{product.category}</CardDescription>
         </CardHeader>
-        <CardContent>
-          <p className="text-2xl font-bold">{product.price}</p>
+        <CardContent className="flex-1">
+          <div className="flex flex-col justify-between h-full">
+            <CardDescription>{product.category}</CardDescription>
+            <p className="text-2xl font-bold">{product.price}</p>
+          </div>
         </CardContent>
       </Card>
     </div>
